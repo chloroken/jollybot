c742c8b63dab4f5b03133c9eb37ab22856f598de
chloroken <chloroken@gmail.com>
2023-03-25 04:24:14 -0400

    add slash commands

--- discord-bot.js
+++ discord-bot.js
@@ -1,14 +1,8 @@
-// Role IDs
-const stewardRole = "1042033712440815626";
-const botRole = "1087284983435100300";
-// Channel IDs
-const botChannel = "1088783744967655454";
-const imgChannel = "1007706705347494019";
-// Commands
-const rolesCommand = "!jollyRoles";
-// Discord.js
-const { Client, GatewayIntentBits, time } = require("discord.js");
-const { token } = require("./config.json");
+// Slash command interfacing
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, time } = require('discord.js');
+const { token } = require('./config.json');
 const client = new Client({
     fetchAllMembers: true,
     intents: [
@@ -19,25 +13,68 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 });
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+// Role IDs
+const adminRole = "1041743581746044938";
+const botRole = "1087284983435100300";
+// Channel IDs
+const imgChannel = "1007706705347494019";
+const zkChannel = "1089095090473029643";
+const botChannel = "1089092588532924447";
+// Commands
+const rolesCommand = "!jollyRoles";
 
 /* Events */
 client.login(token);
-client.on("ready", () => {})
-{
-    client.channels.fetch('1088783744967655454').then(channel => channel.send('I am awake.'))
-}
 client.on("messageCreate", message => {
     console.log("EVENT: messageCreate");
     let m = message, channel = m.channel.id;
-    if ((channel === botChannel) && (isSteward(m)) && (m.content === rolesCommand)) updateRoles(m);
+    if ((channel === botChannel) && (isAdmin(m)) && (m.content === rolesCommand)) updateRoles(m);
     else if ((channel === imgChannel)  && (m.attachments.size === 0)) moderateMessage(message);
+    else if ((channel === zkChannel)  && (!m.content.startsWith("https://zk"))) moderateMessage(message);
 })
 
+/* Slash command event */
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 /* Functions */
-function isSteward(message)
+function isAdmin(message)
 {
     console.log("isSteward()");
-    if (message.member.roles.cache.some(role => role.id === stewardRole)) return(true);
+    if (message.member.roles.cache.some(role => role.id === adminRole)) return(true);
     return(false);
 }
 function updateRoles(message)
